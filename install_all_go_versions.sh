2492723afd2c3b1a7dc9a4596ae314572d8be85d
Jack Lindamood <jack@signalfx.com>
2015-10-13 19:44:23 -0700

    cp

--- install_all_go_versions.sh
+++ install_all_go_versions.sh
@@ -6,15 +6,17 @@ if [ -z "$CIRCLECI" ]; then
 	exit 1
 fi
 
+export GOINTO="/usr/local/gover"
+
 #TODO: Move these checks into vendor?
 install_go_ver() {
-  [ -d /usr/local/gover/go"$1" ] || wget -O - https://storage.googleapis.com/golang/go"$1".linux-amd64.tar.gz | sudo tar -v -C /usr/local/gover/go"$1" -xzf -
+  [ -d $GOINTO/go"$1" ] || wget -O - https://storage.googleapis.com/golang/go"$1".linux-amd64.tar.gz | sudo tar -v -C $GOINTO/go"$1" -xzf -
 }
 
 
 [ -d /usr/local/go ] && sudo mv /usr/local/go /usr/local/go_backup
-mkdir -p /usr/local/gover
+sudo mkdir -p $GOINTO
 install_go_ver 1.5.1
 install_go_ver 1.4.2
 
-ln -s /usr/local/gover/go1.5.1 /usr/local/go
+ln -s $GOINTO/go1.5.1 /usr/local/go
